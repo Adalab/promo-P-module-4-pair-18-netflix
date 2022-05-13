213a0d8ae7b509a7adb99cee3f41fd996671ebf2
Rocio Del Villar <rocio.delvillar@gmail.com>
2022-05-13 11:24:57 +0200

    filter gender ok

--- src/index.js
+++ src/index.js
@@ -13,12 +13,15 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
-
+//endpoint
 server.get('/movies', (req, res) => {
-  const response = {};
+  //Guarda el valor del query param de género en una constante
+  const genderFilterParam = req.query.gender;
   //respuesta de listado pintado  **
   res.json({
     success: true,
-    movies: moviesData.movies,
+    movies: moviesData.movies.filter((item) =>
+      item.gender.includes(genderFilterParam)
+    ),
   });
 });
