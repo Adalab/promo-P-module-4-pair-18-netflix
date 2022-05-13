277f86238ad0039945349061d2a074b51445e1f2
Rocio Del Villar <rocio.delvillar@gmail.com>
2022-05-13 10:34:27 +0200

    paint ok

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+//Importamos con require el JSON
+const moviesData = require('./data/movies.json');
 
 // create and config server
 const server = express();
@@ -10,4 +12,13 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+server.get('/movies', (req, res) => {
+  const response = {};
+  //respuesta de listado pintado  **
+  res.json({
+    success: true,
+    movies: moviesData.movies,
+  });
 });
